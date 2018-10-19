4c5150fd2c0fa52333f3a816ba34caa5d14307bb
Arkadiusz Kusiak <akusiak23@gmail.com>
2018-10-19 10:34:44 -0500

    Login

--- config/routes.js
+++ config/routes.js
@@ -38,16 +38,32 @@ function register(req, res) {
           res.status(201).json(token);
         })
         .catch(err => {
-          res.status(500).json(err)
+          res.status(500).json({ message: 'Something went wrong, please try again.' })
         })
     })
     .catch(err => {
-      res.status(500).json(err)
+      res.status(500).json({ message: 'Something went wrong, please try again.' })
     })
 }
 
 function login(req, res) {
   // implement user login
+  const creds = req.body;
+  
+  db('users')
+    .where({ username: creds.username })
+    .then(user => {
+      const currentUser = user[0];
+      if (creds && bcrypt.compareSync(creds.password, currentUser.password)) {
+        const token = generateToken(currentUser);
+        res.status(200).json({ token });
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Something went wrong, please try again.' })
+    })
 }
 
 function getJokes(req, res) {
